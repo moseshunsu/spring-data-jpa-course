3f8a45f6a72bbbd6070c6ed6ba611bae3eba40d6
Moses Hunsu <hunsumoses7@gmail.com>
2023-05-23 16:18:38 +0100

    created a test unit for illustrating common Spring JPA CRUD Operations.

--- src/main/java/com/higherAchievers/springdatajpacourse/entity/Product.java
+++ src/main/java/com/higherAchievers/springdatajpacourse/entity/Product.java
@@ -1,10 +1,7 @@
 package com.higherAchievers.springdatajpacourse.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -14,6 +11,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
