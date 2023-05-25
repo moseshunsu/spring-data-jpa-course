0e59cfd0182df715fbfbcfe32833cd3c260f6913
Moses Hunsu <hunsumoses7@gmail.com>
2023-05-25 13:47:51 +0100

    added other query or finders methods.

--- src/main/java/com/higherAchievers/springdatajpacourse/repository/ProductRepository.java
+++ src/main/java/com/higherAchievers/springdatajpacourse/repository/ProductRepository.java
@@ -2,8 +2,11 @@ package com.higherAchievers.springdatajpacourse.repository;
 
 import com.higherAchievers.springdatajpacourse.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,5 +18,16 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByNameAndDescription(String name, String description);
     Product findDistinctByName(String name);
     List<Product> findByPriceGreaterThan(BigDecimal price);
+    List<Product> findByPriceLessThan(BigDecimal price);
+    List<Product> findByNameContaining(String name);
+    List<Product> findByNameLike(String name);
+//    @Query("select p from Product p where p.price between ?1 and ?2")
+    List<Product> findByPriceBetween(BigDecimal priceStart, BigDecimal priceEnd);
+    List<Product> findByDateCreatedBetween(LocalDateTime startDate, LocalDateTime endDate);
+//    @Query("select p from Product p where p.name in ?1")
+    List<Product> findByNameIn(Collection<String> names);
+    List<Product> findFirst2ByOrderByNameAsc();
+    List<Product> findTop2ByOrderByPriceDesc();
+
 
 }
--- src/test/java/com/higherAchievers/springdatajpacourse/repository/QueryMethodsTest.java
+++ src/test/java/com/higherAchievers/springdatajpacourse/repository/QueryMethodsTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -76,6 +77,81 @@ public class QueryMethodsTest {
     @Test
     void findByPriceGreaterThanMethod() {
         List<Product> products = productRepository.findByPriceGreaterThan(new BigDecimal(150));
+        products.forEach( product -> {
+            System.out.println(product.getName());
+            System.out.println(product.getDescription());
+        });
+    }
+
+    @Test
+    void findByPriceLessThanMethod() {
+        List<Product> products = productRepository.findByPriceLessThan(new BigDecimal(150));
+        products.forEach( product -> {
+            System.out.println(product.getName());
+            System.out.println(product.getDescription());
+        });
+    }
+
+    @Test
+    void findByNameContainingMethod() {
+        List<Product> products = productRepository.findByNameContaining("2");
+        products.forEach( product -> {
+            System.out.println(product.getName());
+            System.out.println(product.getDescription());
+        });
+    }
+
+    @Test
+    void findByNameLikeMethod() {
+        List<Product> products = productRepository.findByNameLike("product 1");
+        products.forEach( product -> {
+            System.out.println(product.getName());
+            System.out.println(product.getDescription());
+        });
+    }
+
+    @Test
+    void findByPriceBetweenMethod() {
+        List<Product> products = productRepository.findByPriceBetween(new BigDecimal(100),
+                new BigDecimal(300));
+        products.forEach( product -> {
+            System.out.println(product.getName());
+            System.out.println(product.getDescription());
+        });
+    }
+
+    @Test
+    void findByDateCreatedBetweenMethod() {
+        List<Product> products = productRepository.findByDateCreatedBetween(
+                LocalDateTime.of(2023,5,24, 15, 30),
+                LocalDateTime.of(2023,5, 24, 16, 2)
+        );
+        products.forEach( product -> {
+            System.out.println(product.getName());
+            System.out.println(product.getDescription());
+        });
+    }
+
+    @Test
+    void findByNameInMethod() {
+        List<Product> products = productRepository.findByNameIn(List.of("product 1", "product 3"));
+        products.forEach( product -> {
+            System.out.println(product.getName());
+            System.out.println(product.getDescription());
+        });
+    }
+
+    @Test
+    void findFirst2ByOrderByNameAscMethod() {
+        List<Product> products = productRepository.findFirst2ByOrderByNameAsc();
+        products.forEach( product -> {
+            System.out.println(product.getName());
+            System.out.println(product.getDescription());
+        });
+    }
+    @Test
+    void findTop2ByOrderByPriceDescMethod() {
+        List<Product> products = productRepository.findTop2ByOrderByPriceDesc();
         products.forEach( product -> {
             System.out.println(product.getName());
             System.out.println(product.getDescription());
